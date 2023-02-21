14fff98ece877361de2104ef1f31e9504df09a59
Kanisevsky <meltsmokestudio@gmail.com>
2023-02-21 16:28:26 +0000

    Header design

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,12 +1,13 @@
 import React, { useLayoutEffect } from "react";
-import { View, Text, SafeAreaView, Image } from "react-native";
+import { View, Text, SafeAreaView, Image, TextInput } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import {
   ChevronDownIcon,
   UserIcon,
-  AdjustmentsIcon,
-  MagenfyingGlassIcon,
+  AdjustmentsVerticalIcon,
+  MagnifyingGlassIcon,
 } from "react-native-heroicons/outline";
+
 const HomeScreen = () => {
   const navigation = useNavigation();
   useLayoutEffect(() => {
@@ -16,9 +17,9 @@ const HomeScreen = () => {
   }, []);
   return (
     <SafeAreaView className="bg-white pt-5">
-      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
         <Image
-          className="h-7 w-7 bg-gray-300 rouned-full p-4"
+          className="h-7 w-7 bg-gray-300 rounded-full p-4"
           source={{ uri: "https://links.papareact.com/wru" }}
         />
         <View className="flex-1">
@@ -29,6 +30,17 @@ const HomeScreen = () => {
           </Text>
         </View>
         <UserIcon size={35} color="#00CCBB" />
+      </View>
+      {/* Search */}
+      <View className="flex-row items-center pb-2 space-x-2 mx-4 px-4">
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+          <MagnifyingGlassIcon color="gray" size={20} />
+          <TextInput
+            placeholder="Restaurant and cuisines"
+            keyboardType="default"
+          />
+        </View>
+        <AdjustmentsVerticalIcon size={20} color="#00CCBB" />
       </View>
     </SafeAreaView>
   );
